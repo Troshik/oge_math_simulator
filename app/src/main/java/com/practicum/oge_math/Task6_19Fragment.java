740e8ead6a502f2152cc9f49e14f3f411be58c28
Trosha <minitrosha@yandex.ru>
2023-12-15 08:57:28 +0300

    add response handler

--- app/src/main/java/com/practicum/oge_math/Task6_19Fragment.java
+++ app/src/main/java/com/practicum/oge_math/Task6_19Fragment.java
@@ -1,9 +1,12 @@
 package com.practicum.oge_math;
 
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -11,7 +14,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Task6_19Fragment extends Fragment {
 
@@ -38,6 +44,9 @@ public class Task6_19Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_task6_19, container, false);
 
         ImageButton imageLastList = view.findViewById(R.id.imageLastList);
+        Button answBottom = view.findViewById(R.id.answBtn);
+        EditText editText = view.findViewById(R.id.getAnswerEdText);
+        TextView trFlAnsw = view.findViewById(R.id.trFlAnswText);
 
         Fragment TasksList = new TasksFragment();
 
@@ -50,6 +59,21 @@ public class Task6_19Fragment extends Fragment {
                 MainActivity mainActivity = (MainActivity) getActivity();
                 assert mainActivity != null;
                 mainActivity.setActiveFragment();
+            }
+        });
+
+        answBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().equals("")) {
+                    if (editText.getText().toString().equals("3")) {
+                        trFlAnsw.setTextColor(Color.GREEN);
+                        trFlAnsw.setText(R.string.trueAnswer);
+                    } else {
+                        trFlAnsw.setTextColor(Color.RED);
+                        trFlAnsw.setText(R.string.falseAnswer);
+                    }
+                }
             }
         });
 
